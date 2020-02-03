a9543ebada913c9e69eced4eaa40690d86290ba3
IlonaNSiegrist <ilona.siegrist@bluewin.ch>
2020-02-03 16:54:16 +0100

    wip

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.justjava;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TextView;
 
-import android.os.Bundle;
-
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+    int quantity= 1;
+    int basePrise= 50;
 
     @Override
+    //constructor
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+this.quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    public void increment(View View) {
+        this.quantity++;
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+this.quantity);
+    }
+
+    public void decrement(View View){
+        this.quantity--;
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+this.quantity);
     }
 }
